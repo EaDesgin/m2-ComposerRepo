efa9211165e0ad42557da8200795dfbcf211e587
Florin Lavic <florin.lavric@eadesign.ro>
2018-02-23 09:48:24 +0200

    #2053: Commit cu Collection.php si modificarile aduse in fisier.

---
+++ Model/ResourceModel/Collection/Collection.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Eadesigndev\ComposerRepo\Model\ResourceModel\Collection;
+
+use Eadesigndev\ComposerRepo\Model\ComposerRepo as ComposerRepoModel;
+use Eadesigndev\ComposerRepo\Model\ResourceModel\ComposerRepo  as ComposerRepoResourceModel;
+
+class Collection extends AbstractCollection
+{
+
+    /**
+     * @var string
+     */
+    //@codingStandardsIgnoreLine
+    protected $_idComposerName = 'entity_id';
+
+    /**
+     * Init resource model
+     * @return void
+     */
+    public function _construct()
+    {
+
+        $this->_init(
+            ComposerRepoModel::class,
+            ComposerRepoResourceModel::class
+        );
+
+        $this->_map['composer']['entity_id'] = 'main_table.entity_id';
+    }
+
+    /**
+     * Add filter by store
+     *
+     * @param int|array|\Magento\Store\Model\Store $store
+     * @param bool $withAdmin
+     * @return $this
+     */
+    public function addStoreFilter($store, $withAdmin = true)
+    {
+
+        return $this;
+    }
+}
