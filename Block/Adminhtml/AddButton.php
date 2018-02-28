fde50cc4338c735979c2f7cfb305f3496ab45389
Florin Lavic <florin.lavric@eadesign.ro>
2018-02-28 13:12:48 +0200

    #2053: Commit cu buttonul Add new

--- Block/Adminhtml/AddButton.php
+++ Block/Adminhtml/AddButton.php
@@ -2,79 +2,38 @@
 
 namespace Eadesigndev\ComposerRepo\Block\Adminhtml;
 
-use Magento\Backend\Block\Widget\Container;
-use Magento\Backend\Block\Widget\Context;
-use Magento\Customer\Model\Customer;
+use Magento\Backend\Block\Widget\Grid\Container;
 
-/**
- * Class AddButton
- * @package Eadesigndev\ComposerRepo\Block\Adminhtml
- */
 class AddButton extends Container
 {
-
-
     /**
-     * AddButton constructor.
-     * @param Context $context
-     * @param EntityType $entityType
-     * @param array $data
+     * @return void;
      */
-    public function __construct(
-        Context $context,
-        EntityType $entityType,
-        array $data = []
-    ) {
-        $this->entityType = $entityType;
-        parent::__construct($context, $data);
-    }
-
-    /**
-     * @return $this
-     */
-    public function _prepareLayout()
+    public function _construct()
     {
-        $addButtonProps = [
-            'id' => 'add_new_template',
-            'label' => __('Add Template'),
-            'class' => 'add',
-            'button_class' => '',
-            'class_name' => 'Magento\Backend\Block\Widget\Button\SplitButton',
-            'options' => $this->entityOptions(),
-        ];
-        $this->buttonList->add('add_new', $addButtonProps);
 
-        return parent::_prepareLayout();
-    }
+        $this->_controller = 'adminhtml_composerrepo';
+        $this->_blockGroup = 'Eadesigndev_ComposerRepo';
 
-    /**
-     * @return array
-     */
-    public function entityOptions()
-    {
-        $splitButtonOptions = [];
-        $types = $this->entityType->getAvailable();
-
-        foreach ($types as $typeId => $type) {
-            $splitButtonOptions[$typeId] = [
-                'label' => $type,
-                'onclick' => "setLocation('" . $this->entityUrl($typeId) . "')",
-                'default' => Customer::ENTITY == $typeId,
-            ];
-        }
-
-        return $splitButtonOptions;
-    }
-
-    /**
-     * @param $typeId
-     * @return string
-     */
-    private function entityUrl($typeId)
-    {
-        return $this->getUrl(
-            '*/*/newentity',
-            ['type' => $typeId]
+        $this->_headerText = __('PDF Templates');
+        $this->_addButtonLabel = __('Add New Template');
+        parent::_construct();
+        $this->buttonList->add(
+            'template_apply',
+            [
+                'label' => __('Template'),
+                'onclick' => "location.href='" . $this->getUrl('composer_packages/*/edit') . "'",
+                'class' => 'apply'
+            ]
         );
+    }
+
+    /**
+     * @param $resourceId
+     * @return bool
+     */
+    public function _isAllowedAction($resourceId)
+    {
+        return $this->_authorization->isAllowed($resourceId);
     }
 }
