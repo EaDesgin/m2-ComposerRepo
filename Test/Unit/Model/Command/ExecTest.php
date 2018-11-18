9069b682d8078e08f12a6955c18677e36b9e75cd
Lucian Miculescu <office@eadesign.ro>
2018-11-18 21:27:17 +0200

    Added new version and preparing for release

---
+++ Test/Unit/Model/Command/ExecTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lmiculescu
+ * Date: 11/18/18
+ * Time: 8:51 PM
+ */
+
+namespace Eadesigndev\ComposerRepo\Test\Unit\Model\Command;
+
+use Eadesigndev\ComposerRepo\Model\Command\Exec;
+
+class ExecTest extends \PHPUnit_Framework_TestCase
+{
+
+}
